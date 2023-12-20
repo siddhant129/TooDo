0e013c4746e94401413802c894e3bc3b120db24a
siddhant halpatrao <siddhanthalpatrao@gmail.com>
2023-12-20 23:07:19 +0530

    Async handler and validation

--- controller/usersController.js
+++ controller/usersController.js
@@ -4,63 +4,47 @@ const { Users } = require('../Models/users')
 const bcryptjs = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const errorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middlewares/asyncHandler')
 
 const router = express.Router();
 
 // @Get request to get all users
 // @Public method
-exports.getAllUsers = async (req, res, next) => {
-    try {
+exports.getAllUsers = asyncHandler( async (req, res, next) => {
         const allUsers = await Users.find()
         if (allUsers) {
             res.status(200).json({ success: true, Users: allUsers })
         } else {
             return next('No users found', 400)
         }
-    } catch (err) {
-        return next(new errorResponse('No users found', 400))
-    }
-
-}
+})
 
 // @Post request to create new user
 // @Public Method
 
-exports.createUser = async (req, res, next) => {
-    try {
+exports.createUser = asyncHandler(async (req, res, next) => {
         const newUser = new Users({
             userName: req.body.userName,
             email: req.body.email,
             passwordHash: bcryptjs.hashSync(req.body.password, 10),
             image: req.body.image
         })
-        const userData = await Users.findOne().where({ $or: [{ userName: newUser.userName }, { userName: newUser.userName }] }).select('userName')
-
-        if (userData != null) {
-            return next(new errorResponse('Username or email already exists', 400))
-        }
-        else {
             const createdUser = await Users.create(newUser)
             console.log(createdUser);
             if (createdUser) {
                 res.status(200).json({ success: true, Users: createdUser.userName })
             } else {
                 return next(new errorResponse('User not created', 400))
-            }
-        }
-    } catch (err) {
-        return next(new errorResponse(err.message, 400))
-    }
-}
+            } 
+})
 
 // @Post request to log in user and send token
 // @Public method
-exports.loginUser = async (req, res, next) => {
+exports.loginUser = asyncHandler( async (req, res, next) => {
     const userLogData = new Users({
         userName: req.body.user,
         passwordHash: req.body.password
     })
-    try {
         const userData = await Users.findOne().where({ $or: [{ userName: userLogData.userName }, { email: userLogData.userName }] })
         if (userData) {
             bcryptjs.compare(userLogData.passwordHash, userData.passwordHash, (err, data) => {
@@ -83,8 +67,5 @@ exports.loginUser = async (req, res, next) => {
             })
         } else {
             return next(new errorResponse( `${req.body.user} user not found`, 404))
-        }
-    } catch (err) {
-        return next(new errorResponse(err.message, 400))
-    }
-}
+        }  
+})
--- middlewares/error.js
+++ middlewares/error.js
@@ -1,4 +1,17 @@
+const errorResponse = require('../utils/errorResponse')
 const errorHandler = (error, req, res, next)=>{
+
+    if (error.code === 11000) {
+        let message = Object.values(error.keyValue) +' already exists'
+        error = new errorResponse(message, 400)
+    }
+
+    //Mongoose Validation error
+    if (error.name === 'ValidationError') {
+        let message = Object.values(error.errors)
+        error = new errorResponse(message,400)
+    }
+
     res
     .status(error.statusCode || 500)
     .json({success: false, error: error.message||'Internal server error'})
